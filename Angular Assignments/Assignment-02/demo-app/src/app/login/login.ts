9186fae153a6c0a61cfeb2743153c1b47bcad90e
MeetAntala45 <meetantala36@gmail.com>
2026-02-02 22:27:24 +0530

    Angular2

---
+++ Angular Assignments/Assignment-02/demo-app/src/app/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  username: string = 'Meet Antala';
+  password: string = 'Angular123';
+}
