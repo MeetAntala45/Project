0d8e29357881ddd89891619ce288e48c667ce211
MeetAntala45 <meetantala36@gmail.com>
2026-02-02 23:37:36 +0530

    .Net2

---
+++ .Net Core Web API Assignments/Assignment-02/DemoApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age;
+        string name;
+
+        Console.Write("Enter your name: ");
+        name = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        if (age <= 0)
+        {
+            Console.WriteLine("Invalid age entered.");
+        }
+        else
+        {
+            Console.WriteLine("Valid age.");
+        }
+
+        PrintDetails(name, age);
+
+        Console.WriteLine("\nNumbers from 1 to 5:");
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    static void PrintDetails(string userName, int userAge)
+    {
+        Console.WriteLine("\nUser Details:");
+        Console.WriteLine("Name: " + userName);
+        Console.WriteLine("Age: " + userAge);
+    }
+}
